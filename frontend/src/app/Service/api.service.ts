7e1725ef9545488fb6582b47bd755e822ef0273c
Zuhair <zuhair.moinuddin@gmail.com>
2025-08-24 21:19:13 -0400

    initioal

--- frontend/src/app/Service/api.service.ts
+++ frontend/src/app/Service/api.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../environment/environment';
-
+import { environment } from '../environment/environment';
 @Injectable({
   providedIn: 'root'
 })
